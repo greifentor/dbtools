bb8c210570b093f787851e7cc65ead0ce7d54ad3
Oliver Lieshoff <o.lieshoff@schuetze-consulting.ag>
2019-04-11 17:55:46 +0200

    Reformatting.

--- src/main/java/de/ollie/dbtools/DbtoolsApplication.java
+++ src/main/java/de/ollie/dbtools/DbtoolsApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DbtoolsApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(DbtoolsApplication.class, args);
 	}
 
 }
-
